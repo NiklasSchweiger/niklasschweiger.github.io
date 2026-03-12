c94875831186ef29c8ad76ac011bc19aed89c436
NiklasSchweiger <121118707+NiklasSchweiger@users.noreply.github.com>
2026-03-12 11:00:19 +0000

    Deploying to gh-pages from @ NiklasSchweiger/niklasschweiger.github.io@3f961c9f7c36d7271bdaa0411ea94e9a7451d39d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Download my CV as PDF: 🇬🇧 English &amp;nbsp;|&amp;nbsp; 🇩🇪 Deutsch &amp;nbsp;(open link → File → Print → Save as PDF)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
